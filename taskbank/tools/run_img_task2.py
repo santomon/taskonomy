829189865302c53589e2b9e783749aaa96fd735e
lmao <xd>
2020-12-08 21:00:00 +0100

    cli for runimgtask

--- taskbank/tools/run_img_task2.py
+++ taskbank/tools/run_img_task2.py
@@ -147,7 +147,7 @@ def run_to_task():
 
     if args.store_rep:
         s_name, file_extension = os.path.splitext(args.store_name)
-        with open('{}.npy'.format(s_name + '_features' + ("_" + args.encoder_layer if args.save_lname else "")), 'wb') as fp:
+        with open('{}.npy'.format(s_name + '_features' + ("_" + args.encoder_layer.replace("/", "_") if args.save_lname else "")), 'wb') as fp:
             np.save(fp, np.squeeze(representation))
 
     if args.store_pred:
